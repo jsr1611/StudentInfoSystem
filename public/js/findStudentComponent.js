a3de9467376cdee152770a93f2038b59e128cb13
Jumanazar <jsr1611@outlook.com>
2022-07-21 18:36:12 +0900

    updated and fixed some bugs

--- public/js/findStudentComponent.js
+++ public/js/findStudentComponent.js
@@ -2,16 +2,17 @@ function createTemplate(containerId, elementId, title) {
     const template = document.createElement("template");
     template.innerHTML = `
         <div id="${elementId}">
-            <h3>${title}</h3>
+            <h3 id="findOrRemoveTitle">${title}</h3>
             <form id="findForm">
                 <div>
                     <p>Student ID</p>
                     <input type="number" id="studentID" placeholder="Enter student ID">
                 </div>
                 <br>
-                <div>
+                <div class="findBtns">
                     <button id="findBtn">Find</button>
                     <button id="returnHomeBtn">Return Home</button>
+                    <button id="multi-button">Delete</button>
                 </div>
         
             </form>
